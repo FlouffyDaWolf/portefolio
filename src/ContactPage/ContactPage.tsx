21d28722f8d67c303ef572a27ed66d56f2167628
FlouffyDaWolf <evanmideo@hotmail.com>
2026-05-11 14:22:45 +0200

    offr

--- src/ContactPage/ContactPage.tsx
+++ src/ContactPage/ContactPage.tsx
@@ -1,9 +1,20 @@
 import './ContactPage.css';
+import Glow from ".././Decorations/Glow";
+import { usePageInfo } from "./../PageContext";
 
 function ContactPage() {
+    const { setHover } = usePageInfo();
   return (
     <div className="ContactPage">
-      contact
+        <Glow />
+        <div className="Box">
+            <h2>Wanna ask something ?</h2>
+            <h3>I am available at any moment !</h3>
+            <div>
+                <a href= {"https://discord.com/users/545212305353342986"} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}><img src={`${process.env.PUBLIC_URL}/Img/discord.png`} height="60px" width="60px"></img></a>
+                <a href={"mailto: evanmideo@hotmail.com"} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}><img src={`${process.env.PUBLIC_URL}/Img/gmail.png`} height="60px" width="60px"></img></a>
+            </div>
+        </div>
     </div>
   );
 }
